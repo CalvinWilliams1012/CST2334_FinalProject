bfcd621fc013519a86251873795f1b23eea3bbd2
Calvin Williams <calvin.william1012@gmail.com>
2016-11-22 23:02:42 -0500

    Initial Commit

---
+++ app/src/main/java/cst2335/smartenvironment/MainActivity.java
@@ -0,0 +1,62 @@
+package cst2335.smartenvironment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private final String ACTIVITY_NAME = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        Intent intent;
+        switch (id){
+            case R.id.carMenu:
+                Log.i(ACTIVITY_NAME," Car Selected");
+                intent = new Intent(MainActivity.this,Car.class);
+                startActivity(intent);
+                break;
+            case R.id.kitchenMenu:
+                Log.i(ACTIVITY_NAME," Kitchen Selected");
+                intent = new Intent(MainActivity.this,Kitchen.class);
+                startActivity(intent);
+                break;
+            case R.id.livingMenu:
+                Log.i(ACTIVITY_NAME," Living Room Selected");
+                intent = new Intent(MainActivity.this,LivingRoom.class);
+                startActivity(intent);
+                break;
+            case R.id.homeMenu:
+                Log.i(ACTIVITY_NAME," Home Selected");
+                intent = new Intent(MainActivity.this,Home.class);
+                startActivity(intent);
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
